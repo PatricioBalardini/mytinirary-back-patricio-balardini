6b6ae57c0e62016ae32af9cc08d2892952e3e2fa
Patricio Balardini <patriciobalardini@gmail.com>
2023-08-18 06:20:00 +0800

    feat: add controller city

--- models/City.js
+++ models/City.js
@@ -4,14 +4,18 @@ let collection = "cities";
 
 let schema = new Schema({
   country: { type: String, required: true },
-  fundation: { type: Date, required: true },
+  fundation: { type: Date, required: false },
   population: { type: Number },
-  photo: { type: String, required: true },
+  photo: { type: String, required: false },
   city: { type: String, required: true },
-  description: { type: String, default: "edit later" },
-  smalldescription: { type: String, default: "edit later" },
-  featuredLocation: { type: String, default: "edit later" },
-  admin_id: { type: Types.ObjectId, required: true, ref: "users" },
+  description: { type: String, default: "edit later", required: false },
+  smalldescription: { type: String, default: "edit later", required: false },
+  featuredLocation: { type: String, default: "edit later", required: false },
+  admin_id: {
+    type: Types.ObjectId,
+    required: false,
+    ref: "users",
+  },
 });
 
 let City = model(collection, schema);
