e647b0bc10ede752a41a189726c62ae525cf557a
Patricio Balardini <96543246+PatricioBalardini@users.noreply.github.com>
2023-08-21 00:51:29 +0800

    feat: add controller carousel (#4)

--- models/City.js
+++ models/City.js
@@ -2,21 +2,26 @@ import { model, Schema, Types } from "mongoose";
 
 let collection = "cities";
 
-let schema = new Schema({
-  country: { type: String, required: true },
-  fundation: { type: Date, required: false },
-  population: { type: Number },
-  photo: { type: String, required: false },
-  city: { type: String, required: true },
-  description: { type: String, default: "edit later", required: false },
-  smalldescription: { type: String, default: "edit later", required: false },
-  featuredLocation: { type: String, default: "edit later", required: false },
-  admin_id: {
-    type: Types.ObjectId,
-    required: false,
-    ref: "users",
+let schema = new Schema(
+  {
+    country: { type: String, required: true },
+    fundation: { type: Date, required: true },
+    population: { type: Number },
+    photo: { type: String, required: true },
+    city: { type: String, required: true },
+    description: { type: String, default: "edit later", required: true },
+    smalldescription: { type: String, default: "edit later" },
+    featuredLocation: { type: String, default: "edit later" },
+    admin_id: {
+      type: Types.ObjectId,
+      required: true,
+      ref: "users",
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 let City = model(collection, schema);
 export default City;
